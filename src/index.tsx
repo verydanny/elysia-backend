4efcd629c6491a98bda80c72edf1ab29a4e8573b
Daniil Veremchuk <daniel@verydanny.com>
2024-01-03 18:32:31 +0900

    local env

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
 import { user } from './controller/user.js'
 import { html } from '@elysiajs/html'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://toojhcjwevocybrjnsag.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvb2poY2p3ZXZvY3licmpuc2FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwODcxOTgsImV4cCI6MjAxODY2MzE5OH0.5OUhLYCJgi3PSwvQwZCP6aIjGZgrCJeiPIv7SYTsLFo'
+)
 
 const app = new Elysia()
   .use(cors())
